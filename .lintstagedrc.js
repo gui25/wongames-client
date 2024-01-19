956ee630bfc76c6fe68d3762ad7f5d8e49ab14ec
gui25 <gui25ber@gmail.com>
2024-01-19 11:24:52 -0400

    Initial commit from Create Next App

---
+++ .lintstagedrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  // lint & Prettier TS and JS files
+  '*.{js,jsx,ts,tsx}': (filenames) => [
+    `prettier --write ${filenames.join(' ')}`,
+    `npm run lint --fix . ${filenames.join(' --file')}`,
+    `npm test -- --findRelatedTests ${filenames.join(' ')}`
+  ]
+}
